2472e575491e30e037dba326b1dd65f830eb6671
Minji <minjichoe0326@gmail.com>
2023-02-04 20:12:39 +0900

    init
    spring initailizer를 이용하여 스프링부트 프로젝트 생성함

---
+++ src/main/java/com/example/practice_memberService/PracticeMemberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_memberService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMemberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMemberServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice_memberService/PracticeMemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_memberService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMemberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
