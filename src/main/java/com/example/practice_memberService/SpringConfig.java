b80ff97cd8357fab2728c1268426037e0cdbbdb9
Minji <minjichoe0326@gmail.com>
2023-02-08 20:46:47 +0900

    스프링 빈 설정 변경 (순수 Jdbc 이용)
    스프링 컨테이너에 스프링 빈을 메모리 멤버 리포지토리에서 JDBC 멤버 리포지토리로 변경하여 등록하였다.
    - 개방-폐쇄 원칙(OCP, Open-Closed Principle) : 확장에는 열려있고, 수정/변경에는 닫혀있다.
    - 스프링의 DI(Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고, 설정만으로도 클래스를 변경할 수 있다.

--- src/main/java/com/example/practice_memberService/SpringConfig.java
+++ src/main/java/com/example/practice_memberService/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.example.practice_memberService;
 
+import com.example.practice_memberService.repository.JdbcMemberRepository;
 import com.example.practice_memberService.repository.MemberRepository;
 import com.example.practice_memberService.repository.MemoryMemberRepository;
 import com.example.practice_memberService.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+  private DataSource dataSource;
+
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
 
   @Bean
   public MemberService memberService(){
@@ -16,6 +25,7 @@ public class SpringConfig {
 
   @Bean
   public MemberRepository memberRepository(){
-    return new MemoryMemberRepository();
+    // return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 }
