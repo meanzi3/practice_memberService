42f66c2e2c64cc0b108dde41c429ec3ca6867974
Minji <minjichoe0326@gmail.com>
2023-02-09 19:47:36 +0900

    JPA 엔티티 매핑
    Member 객체를 엔티티로 매핑하고 id를 IDENTITY 전략으로 매핑하였다.

--- src/main/java/com/example/practice_memberService/domain/Member.java
+++ src/main/java/com/example/practice_memberService/domain/Member.java
@@ -1,7 +1,13 @@
 package com.example.practice_memberService.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String name;
 
   public Long getId() {
